92d9530b98eefa200296be60cf71425adacc9c5a
marblesx <kotwicki1990@gmail.com>
2020-01-19 12:32:23 -0500

    Update help command back in

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'help',
+    name: 'vhelp',
     description: 'Help command, will give you all the help you need.',
     execute(args, bot, channelID, userID){
         bot.sendMessage({
             to: channelID,
-            message: 'Some commands are: ' + Object.keys( args[2]).map( function(key){ return key }).join(", ")
+            message: 'Some commands are: ' + Object.keys( args[2]).map( function(key){ return key + ":\n" + args[2][key] + "\n" }).join(", ")
         });
     }
 };
