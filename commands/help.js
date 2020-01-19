70074eb48adf1ffbae281693aff10db5b5184242
marblesx <kotwicki1990@gmail.com>
2020-01-19 00:41:01 -0500

    Dynamic help

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,22 @@
 
-const { prefix } = require('../config.json');
+
 
 module.exports = {
     name: 'help',
     description: 'List all of my commands or info about a specific command.',
-    aliases: ['commands'],
-    usage: '[command name]',
-    cooldown: 5,
     execute(args, bot, channelID, userID) {
         const data = [];
         const commands = [];
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:\n');
-            data.push(args[1].map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}[command name].help\` to get info on a specific command!`);
+            data.push(args[1].map(command => command).join(', '));
+            data.push(`\nYou can send \`$![command name].help\` to get info on a specific command!`);
 
             bot.sendMessage({
                 to: channelID,
                 message: data
             });
         }
-    },
+    }
 };
