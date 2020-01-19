820bceac37877296ef8aac3261c1180561c30974
marblesx <kotwicki1990@gmail.com>
2020-01-19 00:16:58 -0500

    Dynamic help

--- commands/help.js
+++ commands/help.js
@@ -1,33 +1,25 @@
 
 const { prefix } = require('../config.json');
 
-const data = [];
-
-
 module.exports = {
     name: 'help',
     description: 'List all of my commands or info about a specific command.',
     aliases: ['commands'],
     usage: '[command name]',
     cooldown: 5,
-    execute(args, bot, channelID, userID){
+    execute(args, bot, channelID, userID) {
         const data = [];
-        const { commands } = args[1].client;
+        const commands = [];
 
         if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
+            data.push('Here\'s a list of all my commands:\n');
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-            return args[1].author.send(data, { split: true })
-                .then(() => {
-                    if (args[1].channel.type === 'dm') return;
-                    args[1].reply('I\'ve sent you a DM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${args[1].author.tag}.\n`, error);
-                    args[1].reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                });
+            bot.sendMessage({
+                to: channelID,
+                message: data
+            });
         }
     },
 };
