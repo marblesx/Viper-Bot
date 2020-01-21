e43a39a586965b4509a43951afcb6739b4c7ba8e
Matthew Kotwicki <matthew.d.kotwicki@gmail.com>
2020-01-20 21:49:56 -0500

    updated with new help code

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
 function getDynamicCommands(commands){
     let returnList = "";
+    console.log(commands);
     commands.forEach(command =>
     returnList+= command.name + ": "+ command.description +"\n"
     );
@@ -10,7 +11,7 @@ module.exports = {
     name: 'vhelp',
     description: 'Help command, will give you all the help you need.',
     execute(args, bot, channelID, userID){
-
+        console.log(args);
         bot.sendMessage({
             to: channelID,
             message: 'Some commands are: \n' +
