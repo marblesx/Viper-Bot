69386376cda7b2cf9108fd9dc8ec0a3886d06332
marblesx <kotwicki1990@gmail.com>
2020-03-24 22:39:27 -0400

    Updated with new method for on add event

--- Common/serverCommon.js
+++ Common/serverCommon.js
@@ -23,5 +23,27 @@
         }
     });
     }
+
+    async function welcomeBot(channel)
+    {request.get({
+        url: `https://api.giphy.com/v1/gifs/search?api_key=${gifToken}&q=welcome&limit=25&offset=0&rating=G&lang=en`,
+        json: true,
+        headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+            console.log(gifToken);
+        } else {
+            // data is already parsed as JSON:
+            if (data.data.length !== 0) {
+                let num = common.getRandomInt(24);
+                channel.send(data.data[num].url);
+            }
+        }
+    });
+    }
     module.exports.welcome = welcome;
+    module.exports.welcomeBot = welcomeBot
 }
