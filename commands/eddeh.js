255701fb8ad1d3119fc6eb561a2ce41c699156c5
marblesx <kotwicki1990@gmail.com>
2021-06-05 11:22:41 -0400

    Added new eddie mod

---
+++ commands/eddeh.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    name: 'eddeh',
+    description: 'Hypes Eddeh up',
+    execute(args, bot) {
+        bot.channel.send('is the finest mod in all the kingdom. All corners of the realm know this as fact');
+    }
+};
